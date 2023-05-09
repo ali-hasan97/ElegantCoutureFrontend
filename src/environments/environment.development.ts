642187474ca806938c2ed61778b660e042199b3d
ali <ali_hasan97@hotmail.com>
2023-05-09 04:48:12 -0500

    deleted environment files

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environmentVars = {
-  production: false,
-  AWS_ACCESS_KEY_ID: 'AKIAXAL332IJYS5YC7HY',
-  AWS_SECRET_ACCESS_KEY: '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',
-  AWS_REGION: 'us-east-2'
-};
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environmentVars = {
-  production: false,
-  AWS_ACCESS_KEY_ID: 'AKIAXAL332IJWYJHLJ63',
-  AWS_SECRET_ACCESS_KEY: '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',
-  AWS_REGION: 'us-east-2'
-};
